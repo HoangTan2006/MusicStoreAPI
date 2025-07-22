88e23a599ab1336bebc684a77c9c2acd37a9873a
HoangTan <181595064+HoangTan2006@users.noreply.github.com>
2025-07-22 19:55:40 +0700

    First commit

---
+++ src/main/java/com/musicstore/musicstoreapi/MusicstoreapiApplication.java
@@ -0,0 +1,13 @@
+package com.musicstore.musicstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstoreapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicstoreapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicstore/musicstoreapi/MusicstoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicstore.musicstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicstoreapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
